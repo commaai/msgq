23ad2563c4ae92f4e111b22c698372de90d69d77
George Hotz <geohot@gmail.com>
2019-11-01 09:31:59 -0700

    import messaging and services

---
+++ messaging/messaging.cc
@@ -0,0 +1,44 @@
+#include "messaging.hpp"
+#include "impl_zmq.hpp"
+
+Context * Context::create(){
+  Context * c = new ZMQContext();
+  return c;
+}
+
+SubSocket * SubSocket::create(){
+  SubSocket * s = new ZMQSubSocket();
+  return s;
+}
+
+SubSocket * SubSocket::create(Context * context, std::string endpoint){
+  SubSocket *s = SubSocket::create();
+  s->connect(context, endpoint);
+
+  return s;
+}
+
+PubSocket * PubSocket::create(){
+  PubSocket * s = new ZMQPubSocket();
+  return s;
+}
+
+PubSocket * PubSocket::create(Context * context, std::string endpoint){
+  PubSocket *s = PubSocket::create();
+  s->connect(context, endpoint);
+  return s;
+}
+
+Poller * Poller::create(){
+  Poller * p = new ZMQPoller();
+  return p;
+}
+
+Poller * Poller::create(std::vector<SubSocket*> sockets){
+  Poller * p = Poller::create();
+
+  for (auto s : sockets){
+    p->registerSocket(s);
+  }
+  return p;
+}
---
+++ messaging/messaging_pyx.pyx
@@ -0,0 +1,92 @@
+# distutils: language = c++
+# cython: c_string_encoding=ascii, language_level=3
+
+from libcpp.string cimport string
+from libcpp cimport bool
+
+
+from messaging cimport Context as cppContext
+from messaging cimport SubSocket as cppSubSocket
+from messaging cimport PubSocket as cppPubSocket
+from messaging cimport Poller as cppPoller
+from messaging cimport Message as cppMessage
+
+
+cdef class Context:
+  cdef cppContext * context
+  def  __cinit__(self):
+    self.context = cppContext.create()
+
+  def __dealloc___(self):
+    del self.context
+
+
+cdef class Poller:
+  cdef cppPoller * poller
+
+  def  __cinit__(self):
+    self.poller = cppPoller.create()
+
+  def __dealloc___(self):
+    del self.poller
+
+  def registerSocket(self, SubSocket socket):
+    self.poller.registerSocket(socket.socket)
+
+  def poll(self, timeout):
+    sockets = []
+
+    result = self.poller.poll(timeout)
+    for s in result:
+        socket = SubSocket()
+        socket.setPtr(s)
+        sockets.append(socket)
+
+    return sockets
+
+cdef class SubSocket:
+  cdef cppSubSocket * socket
+
+  def  __cinit__(self):
+    self.socket = cppSubSocket.create()
+
+  def __dealloc___(self):
+      del self.socket
+
+  cdef setPtr(self, cppSubSocket * ptr):
+    del self.socket
+    self.socket = ptr
+
+  def connect(self, Context context, string endpoint, bool conflate=False):
+    self.socket.connect(context.context, endpoint, conflate)
+
+  def setTimeout(self, int timeout):
+    self.socket.setTimeout(timeout)
+
+
+  def receive(self, bool non_blocking=False):
+    msg = self.socket.receive(non_blocking)
+
+    if msg == NULL:
+      return None
+    else:
+      sz = msg.getSize()
+      m = msg.getData()[:sz]
+      del msg
+
+      return m
+
+
+cdef class PubSocket:
+  cdef cppPubSocket * socket
+  def  __cinit__(self):
+    self.socket = cppPubSocket.create()
+
+  def __dealloc___(self):
+    del self.socket
+
+  def connect(self, Context context, string endpoint):
+    self.socket.connect(context.context, endpoint)
+
+  def send(self, string data):
+    return self.socket.send(<char*>data.c_str(), len(data))
