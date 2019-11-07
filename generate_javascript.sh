ba8f551a975f4e4525a7a4070f7eeda71c0dc71c
Chris Vickery <chrisinajar@gmail.com>
2019-11-07 11:00:40 -0800

    Changes to regex so js bindings can be generated correctly

--- generate_javascript.sh
+++ generate_javascript.sh
@@ -3,8 +3,8 @@
 rm -r gen/ts
 rm -r gen/js
 
-mkdir gen/ts
-mkdir gen/js
+mkdir -p gen/ts
+mkdir -p gen/js
 
 echo "Installing needed npm modules"
 npm i capnpc-ts capnp-ts
@@ -12,14 +12,15 @@ npm i capnpc-ts capnp-ts
 capnpc -o node_modules/.bin/capnpc-ts:gen/ts log.capnp car.capnp
 capnpc -o node_modules/.bin/capnpc-ts:gen/ts car.capnp
 
-cat log.capnp | egrep '\([a-zA-Z]*\.[^\s]+\.[^s]+\)' | sed 's/^.*([a-zA-Z]*\.\([a-zA-Z.]*\)).*/\1/' | while read line
+cat log.capnp | grep '@' | egrep -o '[a-zA-Z]*\.[^\s]+(\.[a-zA-Z]+)+' | sed 's/^[^.]*\.\(.*\)/\1/' | while read line
 do
+  echo $line
   TOKEN=`echo $line | sed 's/\./_/g'`
   ROOT=`echo $line | sed 's/\..*$//g'`
   cat gen/ts/log.capnp.ts | grep '^import.*'${TOKEN}
   if [[ "$?" == "1" ]]
   then
-    sed -i 's/^\(import {.*\)'${ROOT}'\(,*\) \(.*\)$/\1'${ROOT}', '${TOKEN}'\2 \3/' ./gen/ts/log.capnp.ts
+    sed -i '' 's/^\(import {.*\)'${ROOT}'\(,*\) \(.*\)$/\1'${ROOT}', '${TOKEN}'\2 \3/' ./gen/ts/log.capnp.ts
   fi
 done
 
