566152fc29bb6d2c6723395cbc2fc0edc9891e10
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-07-29 14:09:00 -0700

    fix linter

--- messaging/tests/test_services.py
+++ messaging/tests/test_services.py
@@ -25,7 +25,7 @@ class TestServices(unittest.TestCase):
   def test_generated_header(self):
     with tempfile.NamedTemporaryFile(suffix=".h") as f:
       ret = os.system(f"python3 {services.__file__} > {f.name} && clang++ {f.name}")
-      self.assertEqual(ret, 0, f"generated services header is not valid C")
+      self.assertEqual(ret, 0, "generated services header is not valid C")
 
 if __name__ == "__main__":
   unittest.main()
