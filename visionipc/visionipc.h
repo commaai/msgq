728b5ea588187dee11cd33c1b872a2864661f801
Willem Melching <willem.melching@gmail.com>
2020-12-07 13:28:40 +0100

    add extra data struct

--- visionipc/visionipc.h
+++ visionipc/visionipc.h
@@ -1,9 +1,16 @@
 #pragma once
 
 #include <unistd.h>
+#include <cstdint>
 #include "visionbuf.h"
 
 #define VISIONIPC_MAX_FDS 64
+
+struct VIPCBufExtra {
+  uint32_t frame_id;
+  uint64_t timestamp_sof;
+  uint64_t timestamp_eof;
+};
 
 struct VisionIpcPacket {
   size_t idx;
