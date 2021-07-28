6e5d649668e46339f0ed95609b74b4ea0f148791
Willem Melching <willem.melching@gmail.com>
2021-07-28 15:15:06 +0200

    add logging to server

--- visionipc/visionipc_client.cc
+++ visionipc/visionipc_client.cc
@@ -3,10 +3,9 @@
 #include <iostream>
 #include <thread>
 
-#include "ipc.h"
-#include "visionipc_client.h"
-#include "visionipc_server.h"
-
+#include "visionipc/ipc.h"
+#include "visionipc/visionipc_client.h"
+#include "visionipc/visionipc_server.h"
 #include "logger/logger.h"
 
 VisionIpcClient::VisionIpcClient(std::string name, VisionStreamType type, bool conflate, cl_device_id device_id, cl_context ctx) : name(name), type(type), device_id(device_id), ctx(ctx) {
@@ -22,12 +21,11 @@ bool VisionIpcClient::connect(bool blocking){
   connected = false;
 
   // Cleanup old buffers on reconnect
-  bool success = true;
   for (size_t i = 0; i < num_buffers; i++){
-    int r = buffers[i].free();
-    if (r != 0) success = false;
+    if (buffers[i].free() != 0) {
+      LOGE("Failed to free buffer %zu", i);
+    }
   }
-  if (!success) LOGE("Failed to free buffers");
 
   num_buffers = 0;
 
@@ -118,12 +116,11 @@ VisionBuf * VisionIpcClient::recv(VisionIpcBufExtra * extra, const int timeout_m
 
 
 VisionIpcClient::~VisionIpcClient(){
-  bool success = true;
   for (size_t i = 0; i < num_buffers; i++){
-    int r = buffers[i].free();
-    if (r != 0) success = false;
+    if (buffers[i].free() != 0) {
+      LOGE("Failed to free buffer %zu", i);
+    }
   }
-  if (!success) LOGE("Failed to free buffers");
 
   delete sock;
   delete poller;
