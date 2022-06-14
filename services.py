40721c616569c976e75fbe02e5173048ff75f96b
Philip <pkung67@utexas.edu>
2022-06-14 02:22:55 -0500

    adding force feedback service

--- services.py
+++ services.py
@@ -74,6 +74,9 @@ services = {
   "driverEncodeData": (False, 20.),
   "wideRoadEncodeData": (False, 20.),
   "qRoadEncodeData": (False, 20.),
+
+  # conduit
+  "forceFeedback": (True, 0.)
 }
 service_list = {name: Service(new_port(idx), *vals) for  # type: ignore
                 idx, (name, vals) in enumerate(services.items())}
