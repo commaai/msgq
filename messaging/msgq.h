1e01c44a0fc8e19aadf5402f7acff4b9bd62add2
Jason Wen <haibin.wen3@gmail.com>
2022-10-25 02:10:42 -0400

    sunnypilot: msgq: bump num readers to 14

--- messaging/msgq.h
+++ messaging/msgq.h
@@ -5,7 +5,7 @@
 #include <atomic>
 
 #define DEFAULT_SEGMENT_SIZE (10 * 1024 * 1024)
-#define NUM_READERS 12
+#define NUM_READERS 14
 #define ALIGN(n) ((n + (8 - 1)) & -8)
 
 #define UNPACK64(higher, lower, input) do {uint64_t tmp = input; higher = tmp >> 32; lower = tmp & 0xFFFFFFFF;} while (0)
