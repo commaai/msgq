acb24ff8c21d343f05a6eacb061d137144a67a58
Kacper Rączy <gfw.kra@gmail.com>
2023-10-02 15:03:39 -0700

    zmq: assert the same PID (#540)
    Enforce the same PID in ZMQ pub sockets

--- messaging/impl_zmq.h
+++ messaging/impl_zmq.h
@@ -46,6 +46,7 @@ class ZMQPubSocket : public PubSocket {
 private:
   void * sock;
   std::string full_endpoint;
+  int pid = -1;
 public:
   int connect(Context *context, std::string endpoint, bool check_endpoint=true);
   int sendMessage(Message *message);
