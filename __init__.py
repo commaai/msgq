5a0a201b5196a39a4eeeb22828eb1e36482aba02
Justin Newberry <justin@comma.ai>
2023-11-06 18:04:34 -0800

    type checking

--- __init__.py
+++ __init__.py
@@ -1,11 +1,17 @@
 # pylint: skip-file
 import os
 import capnp
+from typing import TYPE_CHECKING
 
 CEREAL_PATH = os.path.dirname(os.path.abspath(__file__))
 capnp.remove_import_hook()
 
-# ruff: noqa: F401
-import cereal.gen.py.car_capnp as car
-import cereal.gen.py.log_capnp as log
-import cereal.gen.py.custom_capnp as custom
+if TYPE_CHECKING:
+  # ruff: noqa: F401
+  import cereal.gen.py.car_capnp as car
+  import cereal.gen.py.log_capnp as log
+  import cereal.gen.py.custom_capnp as custom
+else:
+  log = capnp.load(os.path.join(CEREAL_PATH, "log.capnp"))
+  car = capnp.load(os.path.join(CEREAL_PATH, "car.capnp"))
+  custom = capnp.load(os.path.join(CEREAL_PATH, "custom.capnp"))
