9b573c2be34b638ff462648308d3c7075d0ff174
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-02-11 21:29:31 -0800

    SubMaster: poll a single service (#585)
    * SubMaster: poll a single service
    
    * smarter min/max
    
    * cleanup
    
    ---------
    
    Co-authored-by: Comma Device <device@comma.ai>

--- messaging/tests/test_pub_sub_master.py
+++ messaging/tests/test_pub_sub_master.py
@@ -74,6 +74,28 @@ class TestSubMaster(unittest.TestCase):
       self.assertLess(t, 5)
       self.assertFalse(any(sm.updated.values()))
 
+  def test_avg_frequency_checks(self):
+    for poll in (True, False):
+      sm = messaging.SubMaster(["modelV2", "carParams", "carState", "cameraOdometry", "liveCalibration"],
+                               poll=("modelV2" if poll else None),
+                               frequency=(20. if not poll else None))
+
+      checks = {
+        "carState": (20, 20),
+        "modelV2": (20, 20 if poll else 10),
+        "cameraOdometry": (20, 10),
+        "liveCalibration": (4, 4),
+        "carParams": (None, None),
+      }
+
+      for service, (max_freq, min_freq) in checks.items():
+        if max_freq is not None:
+          assert sm._check_avg_freq(service)
+          assert sm.max_freq[service] == max_freq*1.2
+          assert sm.min_freq[service] == min_freq*0.8
+        else:
+          assert not sm._check_avg_freq(service)
+
   def test_alive(self):
     pass
 
