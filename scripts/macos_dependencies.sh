aa6c09d888d090bcb5d591aa194d11189344b7cc
Maxime Desroches <desroches.maxime@gmail.com>
2024-07-19 22:06:24 -0700

    better

--- scripts/macos_dependencies.sh
+++ scripts/macos_dependencies.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+ROOT=$DIR/../
+
 brew bundle --file=- <<-EOS
 brew "zeromq"
 cask "gcc-arm-embedded"
 brew "gcc@13"
 EOS
+
+if [[ -n "$NO_CATCH2" ]]; then
+  cd /tmp
+  git clone -b v2.x --depth 1 https://github.com/catchorg/Catch2.git
+  cd Catch2
+  mv single_include/* "$ROOT"
+  rm -rf Catch2
+fi
--- scripts/ubuntu_dependencies.sh
+++ scripts/ubuntu_dependencies.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+ROOT=$DIR/../
 
 if [[ ! $(id -u) -eq 0 ]]; then
   if [[ -z $(which sudo) ]]; then
@@ -10,3 +12,11 @@ fi
 
 $SUDO apt-get update
 $SUDO apt-get install -y --no-install-recommends clang opencl-headers libzmq3-dev ocl-icd-opencl-dev cppcheck
+
+if [[ -n "$NO_CATCH2" ]]; then
+  cd /tmp
+  git clone -b v2.x --depth 1 https://github.com/catchorg/Catch2.git
+  cd Catch2
+  mv single_include/* "$ROOT"
+  rm -rf Catch2
+fi
