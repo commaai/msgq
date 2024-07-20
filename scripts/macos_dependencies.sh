56b29bae2eefd85f639441b0d0e0f7ae674b4f3f
Maxime Desroches <desroches.maxime@gmail.com>
2024-07-19 22:29:05 -0700

    ...

--- scripts/macos_dependencies.sh
+++ scripts/macos_dependencies.sh
@@ -12,11 +12,8 @@ fi
 
 # Install brew if required
 if [[ $(command -v brew) == "" ]]; then
-  echo "Installing Homebrew"
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  echo "[ ] installed brew t=$SECONDS"
 
-  # make brew available now
   if [[ $ARCH == "x86_64" ]]; then
       echo 'eval "$(/usr/local/homebrew/bin/brew shellenv)"' >> $RC_FILE
       eval "$(/usr/local/homebrew/bin/brew shellenv)"
