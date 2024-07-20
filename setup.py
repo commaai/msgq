42864201c4a68335aee316a4ff0742faaeac48db
Maxime Desroches <desroches.maxime@gmail.com>
2024-07-19 21:05:14 -0700

    better

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-from setuptools import Command, setup
+from setuptools import Command, setup, Distribution
 from setuptools.command.build import build
 import subprocess
 import os
+
+class BinaryDistribution(Distribution):
+  def has_ext_modules(self):
+    return True
+
 
 class SconsBuild(Command):
   def initialize_options(self) -> None:
@@ -12,14 +17,16 @@ class SconsBuild(Command):
 
   def run(self) -> None:
     scons_flags = '' if 'BUILD_TESTS' in os.environ else '--minimal'
-    subprocess.run([f"scons {scons_flags} -j$(nproc)"], shell=True).check_returncode()
+    subprocess.run([f"scons {scons_flags} -j$(nproc || sysctl -n hw.logicalcpu)"], shell=True).check_returncode()
+
 
 class CustomBuild(build):
   sub_commands = [('scons_build', None)] + build.sub_commands
 
 setup(
     packages = ["msgq", "msgq.visionipc"],
-    package_data={'msgq': ['**/*.cc', '**/*.h', '**/*.pxd', '**/*.pyx', '**/*.so']},
+    package_data={'': ['**/*.cc', '**/*.c', '**/*.h', '**/*.pxd', '**/*.pyx', '**/*.py', '**/*.so', '**/*.npy']},
     include_package_data=True,
-    cmdclass={'build': CustomBuild, 'scons_build': SconsBuild}
+    cmdclass={'build': CustomBuild, 'scons_build': SconsBuild},
+    distclass=BinaryDistribution,
     )
