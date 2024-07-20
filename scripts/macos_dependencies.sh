52360ab8fabc6da235059709d2f43dbab88191fd
Maxime Desroches <desroches.maxime@gmail.com>
2024-07-19 22:24:26 -0700

    brew not in GH???

--- scripts/macos_dependencies.sh
+++ scripts/macos_dependencies.sh
@@ -2,6 +2,18 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 ROOT=$DIR/../
 
+if [[ $(command -v brew) == "" ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+  if [[ $ARCH == "x86_64" ]]; then
+      echo 'eval "$(/usr/local/homebrew/bin/brew shellenv)"' >> $RC_FILE
+      eval "$(/usr/local/homebrew/bin/brew shellenv)"
+  else
+      echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $RC_FILE
+      eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
+fi
+
 brew bundle --file=- <<-EOS
 brew "zeromq"
 cask "gcc-arm-embedded"
