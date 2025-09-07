33c44547f52c1fce6ec744defcb8a7b5518eb3bb
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-09-06 17:19:29 -0700

    mv catch2

--- setup.sh
+++ setup.sh
@@ -25,6 +25,15 @@ else
   echo "WARNING: unsupported platform. skipping apt/brew install."
 fi
 
+# catch2
+if [ ! -d $DIR/msgq/catch2/ ]; then
+  rm -rf /tmp/catch2/ $DIR/msgq/catch2/
+  git clone -b v2.x --depth 1 https://github.com/catchorg/Catch2.git /tmp/catch2
+  pushd /tmp/catch2
+  mv single_include/* $DIR/msgq/
+  popd
+fi
+
 if ! command -v uv &>/dev/null; then
   echo "'uv' is not installed. Installing 'uv'..."
   curl -LsSf https://astral.sh/uv/install.sh | sh
--- test.sh
+++ test.sh
@@ -7,12 +7,6 @@ cd $DIR
 # *** env setup ***
 source ./setup.sh
 
-rm -rf /tmp/catch2/ $DIR/msgq/catch2/
-git clone -b v2.x --depth 1 https://github.com/catchorg/Catch2.git /tmp/catch2
-pushd /tmp/catch2
-mv single_include/* $DIR/msgq/
-popd
-
 # *** build ***
 scons -j8
 
