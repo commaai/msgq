352d61d9085a212bb740cffc749be4c334c0b65a
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-09-06 17:05:21 -0700

    setup

--- test.sh
+++ test.sh
@@ -5,19 +5,7 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 cd $DIR
 
 # *** env setup ***
-if ! command -v uv &>/dev/null; then
-  echo "'uv' is not installed. Installing 'uv'..."
-  curl -LsSf https://astral.sh/uv/install.sh | sh
-
-  # doesn't require sourcing on all platforms
-  set +e
-  source $HOME/.local/bin/env
-  set -e
-fi
-
-export UV_PROJECT_ENVIRONMENT="$DIR/.venv"
-uv sync --all-extras
-source "$DIR/.venv/bin/activate"
+source ./setup.sh
 
 rm -rf /tmp/catch2/ $DIR/msgq/catch2/
 git clone -b v2.x --depth 1 https://github.com/catchorg/Catch2.git /tmp/catch2
