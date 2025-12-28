573f9075c7fa962dcb0ec97a31654d32d3f554dc
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2025-12-28 00:16:33 -0800

    lil more

--- test.sh
+++ test.sh
@@ -18,7 +18,8 @@ codespell -L ned,stdio --builtin clear,rare,informal,usage,code,names,en-GB_to_e
           --skip uv.lock,*_pyx.cpp,catch2*
 
 cppcheck --error-exitcode=1 --inline-suppr --language=c++ \
-         --force --quiet -j4 --check-level=exhaustive
+         --force --quiet -j4 --check-level=exhaustive \
+         --suppress='*:msgq/catch2/*' --suppress='*:*_pyx.cpp' --suppress='*:*_tests.cc' msgq/ visionipc/
 
 # *** test ***
 msgq/test_runner
