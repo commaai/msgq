28080d53f555032f682ec5016bf12ab44c3d7b57
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2026-09-06 10:24:49 -0700

    pip installable from git url (#704)

--- test.sh
+++ test.sh
@@ -12,6 +12,16 @@ scons -j8
 # *** lint + test ***
 lefthook run test
 
+# *** test installed package outside the checkout ***
+(
+  TEST_DIR=$(mktemp -d)
+  trap 'rm -rf "$TEST_DIR"' EXIT
+  uv venv --python "$DIR/.venv/bin/python" "$TEST_DIR/.venv"
+  uv pip install --python "$TEST_DIR/.venv/bin/python" "$DIR"
+  cd "$TEST_DIR"
+  "$TEST_DIR/.venv/bin/python" -m unittest msgq.tests.test_messaging
+)
+
 # *** all done ***
 GREEN='\033[0;32m'
 NC='\033[0m'
